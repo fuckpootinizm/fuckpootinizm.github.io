b54a3afe9ce80913ff6c75864c49285611e88647
bbetperf project fuckpootinizm <128197086+fuckpootinizm@users.noreply.github.com>
2023-03-24 16:33:00 -0700

    upd

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('2 f=\'E-F\';2 5="A g G H I J K L M, N h O P Q R S. T U V W X h Y.";2 i={"Z-10":"11/j",12:`13 ${f}`,};2 6=()=>{2 k=14.15({5:5,16:17,n:1,18:0.7,});19("1a://1b.1c.1d/1e/1f/l-1g-1h/1i",{1j:"1k",1l:i,1m:k,}).m((o)=>o.j()).m((p)=>{2 3=p.1n[0].l;8.9("a-b").c=3;d.1o("3",3)}).1p((e)=>{1q.e(e);2 3=d.q("3");8.9("a-b").c=3})};2 r=()=>{2 s=g 1r();2 t=s.1s("1t-1u",{1v:"1w/1x",});2[u,v]=t.4(", ");2[1y,1z,1A]=u.4("/");2[w,x]=v.4(" ");2[y,z,B]=w.4(":");C(y===«0»&&z===«0»&&B===«0»&&x==="1B"){6()}};2 D=()=>{2 3=d.q("3");C(3){8.9("a-b").c=3}1C{6()}};D();1D(r,1E);',62,103,'||const|antiwarSaying|split|prompt|generateSaying||document|getElementById|antiwar|saying|innerText|localStorage|error|apiKey|new|of|requestHeaders|json|requestData|text|then||response|data|getItem|checkTimeAndGenerate|currentDateTime|losAngelesDateTime|losAngelesDate|losAngelesTime|time|meridiem|hour|minute||second|if|getAntiwarSaying|sk|2f4h7bZT2XnumcuA8h5bT3BlbkFJJwAJWQMJIPo3aJj5csUK|quote|against|the|Putin|regime|in|English|consisting|no|more|than|100|letters|Without|quotation|marks|and|designation|persons|Content|Type|application|Authorization|Bearer|JSON|stringify|max_tokens|30|temperature|fetch|https|api|openai|com|v1|engines|davinci|002|completions|method|POST|headers|body|choices|setItem|catch|console|Date|toLocaleString|en|US|timeZone|America|Los_Angeles|month|day|year|PM|else|setInterval|1000'.split('|'),0,{}))
-const startDate = new Date("February 24, 2022 00:00:00").getTime();
-
-const updateTimer = () => {
-  const now = new Date().getTime();
-  const distance = now - startDate;
-
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  const timerEl = document.getElementById("timer");
-  timerEl.textContent = `days of war – ${days} ' ${hours}:${minutes}:${seconds}`;
-};
-
-setInterval(updateTimer, 1000);
+eval(
+  (function (p, a, c, k, e, r) {
+    e = function (c) {
+      return (
+        (c < a ? "" : e(parseInt(c / a))) +
+        ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
+      );
+    };
+    if (!"".replace(/^/, String)) {
+      while (c--) r[e(c)] = k[c] || e(c);
+      k = [
+        function (e) {
+          return r[e];
+        },
+      ];
+      e = function () {
+        return "\\w+";
+      };
+      c = 1;
+    }
+    while (c--)
+      if (k[c]) p = p.replace(new RegExp("\\b" + e(c) + "\\b", "g"), k[c]);
+    return p;
+  })(
+    'a F=\'1k-1l\';a q="1m 1n 1o 1p 1q\'s G 1r 1s, 1t r 1u 1v 1w H 1x. 1y 1z 1A 1B 1C r 1D.";a I={"1E-1F":"1G/J",1H:`1I ${F}`,};a t=()=>{a K=1J.1K({q:q,1L:H,n:1,1M:0.7,});1N("1O://1P.1Q.1R/1S/1T/L-1U-1V/1W",{1X:"1Y",1Z:I,20:K,}).M((N)=>N.J()).M((O)=>{a e=O.21[0].L;h.i("u-v").k=e;w.22("e",e)}).23((x)=>{25.x(x);a e=w.P("e");h.i("u-v").k=e})};a Q=()=>{a R=y z();a S=R.26("27-28",{29:"2a/2b",});a[T,U]=S.l(", ");a[2c,2d,2e]=T.l("/");a[V,W]=U.l(" ");a[X,Y,Z]=V.l(":");18(X==="0"&&Y==="0"&&Z==="0"&&W==="2f"){t()}};a 19=()=>{a e=w.P("e");18(e){h.i("u-v").k=e}2g{t()}};19();A(Q,f);a 1a=y z("2h 24, 2i B:B:B").1b();a 1c=()=>{a 1d=y z().1b();a j=1d-1a;a C=m.o(j/(f*g*g*24));a 1e=m.o((j%(f*g*g*24))/(f*g*g));a 1f=m.o((j%(f*g*g))/(f*g));a 1g=m.o((j%(f*g))/f);a 1h=h.i("2j");1h.k=`C r 2k 2l 2m 2n 2o G–${C}\' ${1e.D().E(2, "0")}:${1f.D().E(2, "0")}:${1g.D().E(2, "0")} \'`};A(1c,f);a b=["b/c-1.d","b/c-2.d","b/c-3.d","b/c-4.d","b/c-5.d","b/c-6.d","b/c-7.d","b/c-8.d","b/c-9.d","b/c-10.d","b/c-11.d","b/c-12.d","b/c-13.d","b/c-14.d","b/c-15.d","b/c-16.d","b/c-17.d",];2p p=0;a 1i=()=>{p=(p+1)%b.2q;a 1j=h.i("2r");1j.2s=b[p]};A(1i,f);',
+    62,
+    153,
+    "||||||||||const|images|huilo|png|antiwarSaying|1000|60|document|getElementById|distance|textContent|split|Math||floor|currentImage|prompt|of||generateSaying|antiwar|saying|localStorage|error|new|Date|setInterval|00|days|toString|padStart|apiKey|regime|100|requestHeaders|json|requestData|text|then|response|data|getItem|checkTimeAndGenerate|currentDateTime|losAngelesDateTime|losAngelesDate|losAngelesTime|time|meridiem|hour|minute|second|||||||||if|getAntiwarSaying|startDate|getTime|updateTimer|now|hours|minutes|seconds|timerEl|changeImage|imageEl|sk|2f4h7bZT2XnumcuA8h5bT3BlbkFJJwAJWQMJIPo3aJj5csUK|New|quote|only|AGAINST|Putin|in|English|consisting|no|more|than|letters|Without|quotation|marks|and|designation|persons|Content|Type|application|Authorization|Bearer|JSON|stringify|max_tokens|temperature|fetch|https|api|openai|com|v1|engines|davinci|002|completions|method|POST|headers|body|choices|setItem|catch||console|toLocaleString|en|US|timeZone|America|Los_Angeles|month|day|year|PM|else|February|2022|timer|the|war|started|by|putin|let|length|image|src".split(
+      "|"
+    ),
+    0,
+    {}
+  )
+);
